97ec197aa8411bd1331b2bea11e63beb7575d781
Компьютер <@DESKTOP-JR5U4KS>
2024-03-10 15:58:47 +0500

    Добавьте файлы проекта.

---
+++ задание 5/задание 5.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+		std::cout << "variant: ";
+		int var;
+		std::cin >> var;
+		for (int i = 1; i != 10; ++i) std::cout << var << " x " << i << " = " << var * i << '\n';
+		std::cin.ignore();
+		std::cin.get();
+}
